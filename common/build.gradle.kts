ff92a3694beba425b97154cd10a767e28116f0e1
darksaid98 <darksaid9889@gmail.com>
2025-02-19 10:11:16 +0100

    ci: fix maven publishing name

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -32,10 +32,7 @@ deployer {
             url = "https://github.com/milkdrinkers/versionwatch"
         }
 
-        license({
-            name = "GNU General Public License Version 3"
-            url = "https://www.gnu.org/licenses/gpl-3.0.en.html#license-text"
-        })
+        license("GNU General Public License Version 3", "https://www.gnu.org/licenses/gpl-3.0.en.html#license-text")
 
         developer({
             name.set("darksaid98")
